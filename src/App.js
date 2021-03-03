f186f8a469e860295168914b440bd3ae7969f88b
Benito Gonzalez <ohnobenito@gmail.com>
2021-03-02 20:59:12 -0600

    Added function for sorting name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import EmployeeTable from "./components/EmployeeTable/EmployeeTable"
+//import Table from "./components/TestTable";
 import data from "./data/employees.json";
 
-function App() {
-  return (
-    <div>
-    <h1> Hello World! </h1>
-    <EmployeeTable data={data} />;
-    </div>
-    
-  )
+class App extends Component {
+  state = {
+    data
+  };
+  
+  //Sort Function
+  sortName = () => {
+    let data = this.state.data.sort((a, b) => (a.name > b.name) ? 1 : -1)
+    this.setState({data});
+  };
+
+  render() {
+    return (
+      <div>
+      <h1> Hello World! </h1>
+      <EmployeeTable 
+      data={data}
+      sortName={this.sortName}/>
+      </div>
+      
+    )
+  }
+  
 }
 export default App;
