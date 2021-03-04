721fc61740dafeacff8f6adc6c638dff25b1961b
Benito Gonzalez <ohnobenito@gmail.com>
2021-03-03 20:09:47 -0600

    Starting sorting function

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function InputField(props) {
+    const [inputVal, setInputVal] = useState();
+  
+    
+
+     const handleInputChange = (e) => {  
+         setInputVal(
+            e.target.value
+         )
+         console.log(inputVal);
+         let lowerVal = e.target.value.toLowerCase();
+         let newArr = props.data.filter(employee => employee.firstName.toLowerCase().indexOf(lowerVal) !== -1 );
+         props.setFilter(newArr);
+        } 
+    
     return (
-        <div>
-            <form className="form">
-                <input
-                value=""
-                name=""
-                type="text"
-                placeholder="Type to filter search results"
-                />
-            </form>
-        </div>
-    )
+        <form className ="search">
+        <div className="form-group">
+            <label htmlFor="employee">Filter for First Name:</label>
+            <input
+            onChange={(e)=> handleInputChange(e)}
+            name="employee"
+            list="employees"
+            type="text"
+            className="form-control"
+            placeholder="Type in an employee name to begin"
+            id="id"
+           
+            />
+            
+           
+        </div> 
+    </form>
+);
 }
+
 
 export default InputField;
