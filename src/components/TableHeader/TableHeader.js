74e4bc3026eff82f4ba0f82b654c048d708b30c9
Benito Gonzalez <ohnobenito@gmail.com>
2021-03-04 08:06:20 -0600

    Sep components for tableheader

---
+++ src/components/TableHeader/TableHeader.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function TableHeader(props) {
+    let employee = props.employee;
+
+    return (
+        <thead>
+            <tr>
+            <th scope="col"><button
+                type="button"
+                onClick={() => props.sortID()}>ID:</button></th>
+                <th scope="col"><button
+                type="button"
+                onClick={() => props.sortFirstName()}>First Name:</button></th>
+                <th scope="col"><button
+                type="button"
+                onClick={() => props.sortLastName()}>Last:</button></th>
+                <th scope="col"><button
+                type="button"
+                onClick={() => props.sortOccupation()}>Occupation:</button></th>
+                <th scope="col"><button
+                type="button"
+                onClick={() => props.sortLocation()}>Location:</button></th>
+                
+            </tr>
+            </thead>
+    )
+}
+
+export default TableHeader;
